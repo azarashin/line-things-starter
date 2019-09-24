b0c50fd99a4311c15c5cd291093bcab71bbfb1b2
azarashin <azarashin1979@gmail.com>
2019-09-24 12:20:32 +0900

    サービスIDを変更。

--- liff-app/liff.js
+++ liff-app/liff.js
@@ -1,5 +1,5 @@
 // User service UUID: Change this to your generated service UUID
-const USER_SERVICE_UUID         = '66c8ca09-53b7-4a3e-94f2-ee45d6bfa856'; // LED, Button
+const USER_SERVICE_UUID         = '05215121-bcb6-4224-8a1b-3ee64bb91b78'; // LED, Button
 // User service characteristics
 const LED_CHARACTERISTIC_UUID   = 'E9062E71-9E62-4BC6-B0D3-35CDCD9B027B';
 const BTN_CHARACTERISTIC_UUID   = '62FBD229-6EDD-4D1A-B554-5C4E1BB29169';
